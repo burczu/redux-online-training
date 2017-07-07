69fc368605efce6f6c0be1603b5b9bb3f7ff0d1d
burczu <b.dybowski@gmail.com>
2017-07-07 11:34:37 +0200

    initialization

---
+++ src/reducers/eventsReducers.js
@@ -0,0 +1,15 @@
+import * as constants from '../constants';
+import events from '../data/events.json';
+
+const initialState = {
+  events
+};
+
+export function eventsReducer(state = initialState, action) {
+  switch (action.type) {
+    case constants.EVENTS_CLEAR_LIST:
+      return { ...state, events: [] };
+    default:
+      return state;
+  }
+}
