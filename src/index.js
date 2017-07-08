b077b244256b064d0aac81d8690aa22bbe1951e7
burczu <b.dybowski@gmail.com>
2017-07-08 13:11:06 +0200

    index init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
 
